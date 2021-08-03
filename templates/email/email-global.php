1358e2ed16d65350b3c64baa56501ef573a51587
Paul Gillespie <paul@paulandsam.co>
2021-08-03 18:17:52 +0100

    Add files via upload

--- templates/email/email-global.php
+++ templates/email/email-global.php
@@ -1,1 +1,47 @@
+<table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="margin:0;padding:0;background-color:#f7f7f7;font-family:Helvetica;border-collapse:collapse;height:100%;width:100%">
+    <tbody>
+        <tr>
+            <td align="center" valign="top" style="margin:0;padding:20px 20px 50px 20px;height:100%;width:100%">
+                <table width="600" border="0" cellpadding="0" cellspacing="0" style="width:600px;background-color:#ffffff;border-collapse:collapse;border-bottom:1px solid #e2e2e2;">
+                    <tbody>
+                        <tr>
+                            <td align="center" valign="top">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color:#f7f7f7;border-collapse:collapse">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center" valign="top" style="padding-top:40px;padding-bottom:40px;">
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr border="0" cellpadding="0" cellspacing="0" width="100%" style="border:1px solid #e2e2e2;border-width:1px 1px 0 1px;border-collapse:collapse">
+                            <td align="center" valign="top" style="color:#444444;font-family:Helvetica;font-size:16px;line-height:160%;padding-top:30px;padding-right:40px;padding-bottom:30px;padding-left:40px">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse">
+                                    <tbody>
+                                        <tr>
+                                            <td align="left">
+                                                <span style="color:#555555;font-size:18px;line-height:120%"><h1 style="font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;color:#333333;font-size:20px;margin:0 0 20px">__EMAILTITLE__</h1></span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr border="0" cellpadding="0" cellspacing="0" width="100%" style="border:1px solid #e2e2e2;border-width:1px 1px 0 1px;border-collapse:collapse">
+                            <td align="center" valign="top" style="color:#444444;font-family:Helvetica;font-size:16px;line-height:160%;padding-top:30px;padding-right:40px;padding-bottom:30px;padding-left:40px">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse">
+                                    <tbody>
+                                        __USERFIELDS__
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
